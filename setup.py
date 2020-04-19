8c9e21f8f386a3548960bd3f6a01aba6a6fbfcf6
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2020-04-19 19:05:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-logging',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
-    author='Alexandr Kabakov',
+    author='Dmitry Kalinin',
     url='https://github.com/null-none/django-logging'
 )
